2a8c695b514a8185e37a4158e89078b6c72ed764
NFH-JHTM <vietnamneverdie1@gmail.com>
2025-03-04 11:24:04 +0700

    Update script.js

--- script.js
+++ script.js
@@ -128,3 +128,26 @@ window.addEventListener("resize", () => {
     particlesCanvas.height = window.innerHeight;
 });
 
+
+function typeWriterEffect(element, text, speed) {
+    let i = 0;
+    function type() {
+        if (i < text.length) {
+            element.innerHTML += text.charAt(i);
+            i++;
+            setTimeout(type, speed);
+        }
+    }
+    element.innerHTML = "";
+    type();
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+    const title = document.querySelector("h1");
+    typeWriterEffect(title, "Chào Mừng Các Quý Cô", 100);
+
+    
+    const subtitle = document.querySelector("p");
+    subtitle.classList.add("fly-in");
+});
+
